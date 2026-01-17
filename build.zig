30d6dd30dfaa27b84b95021cda4b7fdcb8437b38
Peter Hellberg <peter@c7.se>
2026-01-18 00:55:10 +0100

    build: src/fly.zig instead of src/main.zig

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) !void {
     const exe = b.addExecutable(.{
         .name = id,
         .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
+            .root_source_file = b.path("src/fly.zig"),
             .target = b.resolveTargetQuery(.{
                 .cpu_arch = .wasm32,
                 .os_tag = .freestanding,
